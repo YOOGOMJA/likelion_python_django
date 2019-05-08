0e4856cb16605f8f8032eeccd28a39a173cb5874
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-05-08 16:29:36 +0900

    first commit
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

---
+++ main.py
@@ -0,0 +1,14 @@
+members = [
+    [ 'yoo' , '2016101452' ],
+    [ 'yoo2' , '2016101452' ],
+    [ 'yoo3' , '2016101452' ],
+    [ 'yoo4' , '2016101452' ]
+]
+
+# for(i = 0 ; i < members.count ; i++){
+#     print(members[0][1] , ' ' , members[0][2]);
+# }
+
+for member in members:
+    print(member[0] + ' ' + member[1])
+
